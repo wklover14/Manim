ea8e5568453197167cea446f9a00bf718c7ce8e0
gwatchok <gwatchok@etu.utc.fr>
2024-12-19 15:06:17 +0100

    add some animations + line

--- scene.py
+++ scene.py
@@ -7,6 +7,11 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
 
+        # Shift the circle
+        self.wait(1)
+        circle.shift(RIGHT)
+        self.wait()
+
 
 class SquareToCircle(Scene):
     def construct(self):
@@ -66,3 +71,47 @@ class DifferentRotations(Scene):
             Rotate(right_square, angle=PI),
             run_time = 2
         )
+
+class WeirdSquare(Scene):
+    def construct(self):
+        square = Square()
+
+        self.play(FadeIn(square))
+        square.set_fill(PURE_GREEN)
+
+        self.play(
+            square.animate.set_fill(PURE_GREEN).shift(3 * UP).rotate(PI / 4)
+        )
+        self.wait(1)
+
+        self.play(
+            FadeOut(square)
+        )
+
+        self.wait(2)
+
+
+class MobjectExample(Scene):
+    def construct(self):
+        p1 = [-1,-1, 0]
+        p2 = [ 1,-1, 0]
+        p3 = [ 1, 1, 0]
+        p4 = [-1, 1, 0]
+        a  = Line(p1,p2).append_points(Line(p2,p3).points)
+
+        point_start  = a.get_start()
+        point_end    = a.get_end()
+        point_center = a.get_center()
+
+        self.add(Text(f"a.get_start() = {np.round(point_start,2).tolist()}", font_size=24).to_edge(UR).set_color(YELLOW))
+        self.add(Text(f"a.get_end() = {np.round(point_end,2).tolist()}", font_size=24).next_to(self.mobjects[-1],DOWN).set_color(RED))
+        self.add(Text(f"a.get_center() = {np.round(point_center,2).tolist()}", font_size=24).next_to(self.mobjects[-1],DOWN).set_color(BLUE))
+
+        self.add(Dot(a.get_start()).set_color(YELLOW).scale(2))
+        self.add(Dot(a.get_end()).set_color(RED).scale(2))
+        self.add(Dot(a.get_top()).set_color(GREEN_A).scale(2))
+        self.add(Dot(a.get_bottom()).set_color(GREEN_D).scale(2))
+        self.add(Dot(a.get_center()).set_color(BLUE).scale(2))
+        self.add(Dot(a.point_from_proportion(0.5)).set_color(ORANGE).scale(2))
+        self.add(*[Dot(x) for x in a.points])
+        self.add(a)
