de1791eff87db3b55bf0398b8348155896f66efd
gwatchok <gwatchok@etu.utc.fr>
2024-12-29 10:43:35 +0100

    Second try with 2D graphs

--- Youtube/graph.py
+++ Youtube/graph.py
@@ -1,38 +1,56 @@
 from manim import *
 from math import sin, cos
 
-class ParametricSinCurve(Scene):
+class ExampleFunctionGraph(Scene):
     def construct(self):
-        axes = NumberPlane()
-        axes.add_coordinates()
-        self.add(axes)
+        # Axes creation
+        axes = Axes((-10, 10), (-5, 5)).add_coordinates()
+        axes.shift(LEFT).scale(1.25)
+        labels = axes.get_axis_labels(Tex("x-axis").scale(1), Tex("y-axis").scale(1))
 
-        sin_curve = ParametricFunction(
-            function= lambda t: [
-                t,
-                sin(t),
-                0
-            ],
-            t_range=[-5, 5, 0.1]
-        )
+        # function to plot
+        exp_func = lambda t: [t, np.exp(t), 0]
+        ln_func  = lambda t: [t, np.log2(t), 0]
 
-        cos_curve = ParametricFunction(
-            function= lambda t: [
-                t,
-                cos(t),
-                0
-            ],
-            t_range=[-5, 5, 0.1]
-        )
+        exp_graph = axes.plot_parametric_curve(exp_func, color=BLUE, t_range=[-10, 10]) # Get the corresponding function graph
+        ln_graph = axes.plot_parametric_curve(ln_func, color=GREEN, discontinuities=[0], dt=0.1, t_range=[0.01, 10])
 
-        sin_curve.set_color(YELLOW)
+        a = exp_graph.get_point_from_function(t=0)
+        b = ln_graph.get_point_from_function(t=2)
 
-        p_cos_pi_4 = axes.c2p(PI/4, cos(PI/4), 0)
-        p_sin_pi_6 = axes.c2p(PI/6, sin(PI/6), 0)
+        self.add(axes, labels)
+        self.play(Create(exp_graph))
+        self.play(Create(Dot(a)))
+        self.wait(1)
 
-        self.play(Create(sin_curve))
-        self.play(Create(cos_curve))
+        self.play(Transform(exp_graph,ln_graph), run_time=2)
+        self.play(Transform(Dot(a),Dot(b)))
 
-        self.add(Dot(p_cos_pi_4))
-        self.add(Dot(p_sin_pi_6))
-        self.wait(3)
+        self.play(Create(axes.get_vertical_line(b)))
+        self.play(Create(axes.get_horizontal_line(b)))
+
+        self.wait(5)
+
+
+class PlotParametricFunction(Scene):
+    def func(self, t):
+        return (np.sin(2 * t), np.sin(3 * t), 0)
+
+    def construct(self):
+        func = ParametricFunction(self.func, t_range = (0, TAU), fill_opacity=0).set_color(RED)
+        self.add(func.scale(3))
+
+
+class ThreeDParametricSpring(ThreeDScene):
+    def construct(self):
+        curve1 = ParametricFunction(
+            lambda u: (
+                1.2 * np.cos(u),
+                1.2 * np.sin(u),
+                u * 0.05
+            ), color=RED, t_range = (-3*TAU, 5*TAU, 0.01)
+        ).set_shade_in_3d(True)
+        axes = ThreeDAxes()
+        self.add(axes, curve1)
+        self.set_camera_orientation(phi=80 * DEGREES, theta=-60 * DEGREES)
+        self.wait()
